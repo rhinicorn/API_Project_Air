4c75765b8423f2efc4dc5faf815657cc36801726
Rhinicorn <admin@rhinicorn.com>
2021-05-08 22:43:13 -0400

    final revisions

--- public/index.js
+++ public/index.js
@@ -1,17 +1,10 @@
 
 const allURL = 'http://api.airvisual.com/v2/nearest_city?key=eb088148-0536-422c-b8f2-da7ef03d5245'
 
-const baseNearCityURL = 'http://api.airvisual.com/v2/nearest_city?key=eb088148-0536-422c-b8f2-da7ef03d5245'
-
-const baseStatesURL = 'http://api.airvisual.com/v2/states?country={USA}&key={eb088148-0536-422c-b8f2-da7ef03d5245}'
-
-const baseCityURL = 'http://api.airvisual.com/v2/cities?state={{STATE_NAME}}&country={USA}&key={eb088148-0536-422c-b8f2-da7ef03d5245}'
-
 
 //NEW RESULTS ARRAY
 let ResultsArr = []
-let resultsLog = document.getElementById("results-log")
-
+let results_Log = document.getElementById("resultsLog")
 
 //FETCH FOR RESULTS
 function fetchData(e){
@@ -24,7 +17,6 @@ function fetchData(e){
             displayResults(allData);
         })
 }
-
 
 //DISPLAY RESULTS
 function displayResults(Data){
@@ -39,39 +31,104 @@ function displayResults(Data){
     ResultsArr.push(myresults)
     renderResults(ResultsArr)
 }
+//CREATE THE TABLE RENDERING
+function renderResults(Data){
+    console.log(ResultsArr)
+    results_Log.innerHTML = ""
+        for(let i = 0; i<ResultsArr.length; i++){
+            let row = createTableRow(ResultsArr[i])
+            results_Log.appendChild(row)
+        }
+}
+//ADD ROWS USING FETCHED DATA
+function createTableRow(e) {
+    console.log(e)
+    
+    let tableRow = document.createElement('tr')
+    let tCity = document.createElement('td')
+    let tCountry = document.createElement('td')
+    let tTs = document.createElement('td')
+    let taqius = document.createElement('td')
+    let tmainus = document.createElement('td')
+    
+    tCity.innerText = e.tCity
+    tCountry.innerText = e.tCountry
+    tTs.innerText = e.tTs
+    taqius.innerText = e.taqius
+    tmainus.innerText = e.tmainus
+  
+    tableRow.appendChild(tCity)
+    tableRow.appendChild(tCountry)
+    tableRow.appendChild(tTs)
+    tableRow.appendChild(taqius)
+    tableRow.appendChild(tmainus)
+  
+    return tableRow
+  }
 
-//CREATE THE TABLE RENDERING FOR GLOBAL
-function renderResults(ResultsArr){
-  console.log(ResultsArr)
-  resultsLog.innerHTML = ""
-    for(let i = 0; i<ResultsArr.length; i++){
-        let row = createTableRow(ResultsArr[i])
-        resultsLog.appendChild(row)
+
+
+
+
+//NEW FORECAST ARRAY
+let ResultsArr2 = []
+let results2_Log = document.getElementById("results2Log")
+
+//DISPLAY RESULTS2
+function displayResults2(Data){
+    //console.log(Data)
+    let myresults2 = {
+        tCity: Data.city,
+        tCountry: Data.country,
+        tTs: Data.current.weather.ts,
+        thumidity: Data.current.weather.hu,
+        tpressure: Data.current.weather.pr,
+        twinddir: Data.current.weather.wd,
+        twindspeed: Data.current.weather.ws
     }
+    ResultsArr2.push(myresults2)
+    renderResults2(ResultsArr2)
+}
+
+//CREATE THE TABLE RENDERING
+function renderResults2(Data){
+    //console.log(ResultsArr2)
+    results2_Log.innerHTML = ""
+        for(let i = 0; i<ResultsArr2.length; i++){
+            let row = createTableRow2(ResultsArr2[i])
+            results2_Log.appendChild(row)
+        }
 }
 
 //ADD ROWS USING FETCHED DATA
-function createTableRow(e) {
-  console.log(e)
-  
-  let tableRow = document.createElement('tr')
-  let tCity = document.createElement('td')
-  let tCountry = document.createElement('td')
-  let tTs = document.createElement('td')
-  let taqius = document.createElement('td')
-  let tmainus = document.createElement('td')
-  
-  tCity.innerText = e.tCity
-  tCountry.innerText = e.tCountry
-  tTs.innerText = e.tTs
-  taqius.innerText = e.taqius
-  tmainus.innerText = e.tmainus
+function createTableRow2(e) {
+    console.log(e)
 
-  tableRow.appendChild(tCity)
-  tableRow.appendChild(tCountry)
-  tableRow.appendChild(tTs)
-  tableRow.appendChild(taqius)
-  tableRow.appendChild(tmainus)
+    let tableRow = document.createElement('tr')
+    let tCity = document.createElement('tCity1')
+    let tCountry = document.createElement('tCountry1')
+    let tTs = document.createElement('tTs1')
+    let thumidity = document.createElement('thumidity1')
+    let tpressure = document.createElement('tpressure1')
+    let twinddir = document.createElement('twinddir1')
+    let twindspeed = document.createElement('twindspeed1')
+    
+    tCity.innerText = e.tCity
+    tCountry.innerText = e.tCountry
+    tTs.innerText = e.tTs
+    thumidity.innerText = e.thumidity
+    tpressure.innerText = e.tpressure
+    twinddir.innerText = e.twinddir
+    twindspeed.innerText = e.twindspeed
 
-  return tableRow
-}
+  
+    tableRow.appendChild(tCity)
+    tableRow.appendChild(tCountry)
+    tableRow.appendChild(tTs)
+    tableRow.appendChild(thumidity)
+    tableRow.appendChild(tpressure)
+    tableRow.appendChild(twinddir)
+    tableRow.appendChild(twindspeed)
+  
+    return tableRow
+  }
